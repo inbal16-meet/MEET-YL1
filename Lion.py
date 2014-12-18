0bda1ae4221d97b4e7ada7dbdca5747be7201934
Inbal Ilsar <inbal16@meet.mit.edu>
2014-12-18 19:58:26 +0200

    That went badly

---
+++ Lion.py
@@ -0,0 +1,11 @@
+
+print("Choose a number")
+x= input()
+if (int(x)==7):
+    print("YAY! My lucky number!")
+elif (int(x)==1999):
+	print("Almost correct...Keep trying!")
+else:   
+	print("WRONG ANSWER") 
+
+
---
+++ divisors.py
@@ -0,0 +1,7 @@
+
+
+a=input("put in a number please!")
+n=int(a) 
+for number in range(1,n+1):
+	if a%number==0:
+		print(number)
