ac26ac23d774c1b73e462765cbcb6a8432f7f541
Amitay Cohen <amitay16@meet.mit.edu>
2015-01-15 20:04:33 +0200

    oop.

---
+++ oop.py
@@ -0,0 +1,29 @@
+class Animal:
+	def __init__ (self,name,age,color,size):
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size
+
+
+
+
+
+
+Dog=Animal("Sadek",22,"Yellow","Very Big")
+
+print(Dog.name)
+print(Dog.age)
+
+
+
+
+
+
+
+
+
+#functions= methods. 
+#first word in "class" is def
+#first input is "init" , then "self"
+
